f0a771f0574f920d11a53b3b67a616494b2ba0c9
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2023-08-11 09:17:50 -0600

    validacines en create

--- app/controller/create.php
+++ app/controller/create.php
@@ -4,6 +4,30 @@
         let telefono = $("#telefono").val()
         let email = $("#email").val()
 
+        if( nombre == null || nombre.length == 0 || (/^\s+$/.test(nombre)) ) {
+            return Swal.fire({
+                title: 'Fallo',
+                text: "Ingresa un nombre valido!",
+                icon: 'warning'     
+            })
+        }
+
+        if(telefono == null || telefono.length == 0 || !(/^\d{10}$/.test(telefono)) ) {
+            return Swal.fire({
+                title: 'Fallo',
+                text: "Ingresa un numero valido!",
+                icon: 'warning'     
+            })
+        }
+
+        if(email == null || email.length == 0 ||  !(/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(email))) {
+            return Swal.fire({
+                title: 'Fallo',
+                text: "Ingresa un email valido!",
+                icon: 'warning'     
+            })
+        }
+
         $.ajax({
             url: './app/model/process/create.php',
             data: {
